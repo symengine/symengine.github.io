168ccc0f04a3b7f8244e4d3ed3d95b457bc04b61
Rohit Goswami <rgoswami@ieee.org>
2020-10-01 16:32:32 +0000

    shell: Init doxyrest

--- shell.nix
+++ shell.nix
@@ -3,6 +3,15 @@ let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
   customPython = pkgs.poetry2nix.mkPoetryEnv { projectDir = ./.; };
+  doxyrest =
+    pkgs.callPackage ./nix/pkgs/doxyrest.nix { pythonEnv = customPython; };
 in pkgs.mkShell {
-  buildInputs = with pkgs; [ doxygen customPython rake lcov darkhttpd ];
+  buildInputs = with pkgs; [
+    doxygen
+    customPython
+    rake
+    lcov
+    darkhttpd
+    doxyrest
+  ];
 }
